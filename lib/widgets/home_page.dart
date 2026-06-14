77e7ba512210189200f244a675475ace7dc22505
BeingIsA <2321409910@qq.com>
2026-06-14 14:40:01 +0800

    调整目录

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phro/notifiers/chat_history_notifier.dart';
 import 'package:phro/notifiers/active_chat_notifier.dart';
-import 'package:phro/widgets/message_input.dart';
+import 'package:phro/widgets/chat/message_input.dart';
 import 'package:phro/services/chat_service.dart';
 import 'package:phro/models/chat.dart';
 import 'package:phro/models/message.dart';
